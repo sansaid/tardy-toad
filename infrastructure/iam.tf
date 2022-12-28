06d48ab72aa9f5a4f1d4762daaf8b319344ec3bc
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-12-28 02:36:47 +0000

    Fixing bugs in initial infrastructure code

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -15,5 +15,5 @@ resource "google_service_account" "tardy-toad-bot" {
 
 resource "google_service_account_iam_policy" "tardy-toad-bot" {
   service_account_id = google_service_account.tardy-toad-bot.name
-  policy_data        = data.google_iam_policy.admin.policy_data
+  policy_data        = data.google_iam_policy.tardy-toad-bot.policy_data
 }
